ec77fd47fdea7c6d4ffe77198489b08d64d815d1
Parthvi Patel <parthvi@Parthvis-MacBook-Pro.local>
2017-03-19 12:02:18 -0700

    Redirect after POST and show all items in template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -27,39 +27,37 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')  
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')  
+        inputbox.send_keys(' Buy peacock feathers')  
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)  
+        time.sleep(3)  
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 	    # There is still a text box inviting her to add another item. She
     	# enters "Use peacock feathers to make a fly" (Edith is very
     	# methodical)
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
+
+
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(' Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
     	# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
 	    # Edith wonders whether the site will remember her list. Then she sees
 	    # that the site has generated a unique URL for her -- there is some
-    # explanatory text to that effect.
+        # explanatory text to that effect.
         self.fail('Finish the test!')
 
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,10 +7,19 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-    	response = self.client.post('/', data={'item_text': 'A new list item'})
-    	self.assertIn('A new list item', response.content.decode())
-    	self.assertTemplateUsed(response,'home.html')
+
+    def test_only_saves_items_when_necessary(self):
+    	self.client.get('/')
+    	self.assertEqual(Item.objects.count(),0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 	
 		# refactoring of test_uses_home_template
 		# from django.core.urlresolvers import resolve
@@ -32,6 +41,7 @@ class HomePageTest(TestCase):
 		#      self.assertTrue(html.endswith('</html>'))  
 		#      self.assertTemplateUsed(response ,'home.html')
 
+
 class ItemModelTest(TestCase):
 
 	def test_saving_and_retriving_items(self):
@@ -51,6 +61,16 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text , 'first saved item')
 		self.assertEqual(second_saved_item.text , 'second saved item')
 
+	def test_can_save_a_POST_request(self):
+		self.client.post('/',data={'item_text':'A new list item'})
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text,'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/',data ={'item_text':'A new list item'})
+		self.assertEqual(response.status_code,302)
+		self.assertEqual(response['location'],'/')
 
 
 

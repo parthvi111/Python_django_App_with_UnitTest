7d2a72feb67367702a2cd6dea75fe5773dfc62b5
Parthvi Patel <parthvi@Parthvis-MacBook-Pro.local>
2017-03-19 13:03:00 -0700

    FT is working well

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,13 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -11,15 +15,28 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self , row_text):
-    	table = self.browser.find_element_by_id('id_list_table')
-    	rows = table.find_elements_by_tag_name('tr')
-    	self.assertIn(row_text ,[row.text for row in rows])
+
+
+    def wait_for_row_in_list_table(self,row_text):
+        MAX_WAIT = 10
+
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except(AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -37,8 +54,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)  
-        time.sleep(3)  
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 	    # There is still a text box inviting her to add another item. She
     	# enters "Use peacock feathers to make a fly" (Edith is very
@@ -48,11 +64,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys(' Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
 
     	# The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
 	    # Edith wonders whether the site will remember her list. Then she sees
@@ -60,6 +75,3 @@ class NewVisitorTest(unittest.TestCase):
         # explanatory text to that effect.
         self.fail('Finish the test!')
 
-
-if __name__ == '__main__':
-	unittest.main(warnings = 'ignore')

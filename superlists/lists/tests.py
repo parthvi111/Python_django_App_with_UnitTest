74fa98c6d713dad22e9c950456ce1fa47532e779
Parthvi Patel <parthvi@Parthvis-MacBook-Pro.local>
2017-03-18 21:25:08 -0700

    refactore functional_test file

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,6 +6,12 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+    	response = self.client.post('/', data={'item_text': 'A new list item'})
+    	self.assertIn('A new list item', response.content.decode())
+    	self.assertTemplateUsed(response,'home.html')
+	
+# refactoring of test_uses_home_template
 # from django.core.urlresolvers import resolve
 # from django.test import TestCase
 # from lists.views import home_page 
@@ -24,3 +30,6 @@ class HomePageTest(TestCase):
 #         self.assertIn('<title>To-Do lists</title>', html)  
 #         self.assertTrue(html.endswith('</html>'))  
 #         self.assertTemplateUsed(response ,'home.html')
+
+
+

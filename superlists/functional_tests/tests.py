c1fdf1da7393a9f8b328edd8c14425d51173d865
Parthvi Patel <parthvi@Parthvis-MacBook-Pro.local>
2017-03-19 16:15:28 -0700

    New URL

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -75,3 +75,53 @@ class NewVisitorTest(LiveServerTestCase):
         # explanatory text to that effect.
         self.fail('Finish the test!')
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #Edith strat a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(' Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        #she notice that her list has unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
+
+        #now new user Francis comes along to the site
+
+        # we use a new browser session to make sure that no information
+        # of Edith's is coming through from cookies etc
+
+        self.browser.quit()
+
+
+
+        self.browser = webdriver.Firefox()
+        # Francis visits the home page. There is no sign of Edith's lists
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn(' Buy peacock feathers',page_text)
+        self.assertNotIn(' make a fly',page_text)
+
+        # Francis start to add items in his own list .He is less interesting then edith
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(' Buy Milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy Milk')
+
+        #Francis getting his own url
+        Francis_list_url = self.browser.current_url
+        self.assertRegex(Francis_list_url,'/lists/.+')
+        self.assertNotEqual(Francis_list_url,edith_list_url)
+
+        # Again there is no trace of edith's url
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn(' Buy peacock feather',page_text)
+        self.assertNotIn(' Buy Milk',page_text)
+
+        #satisfied they both go to sleep
+
+
+
+
+

04a527dd0345b88359c35e232257a14e275b4fce
Parthvi Patel <parthvi@Parthvis-MacBook-Pro.local>
2017-03-18 21:44:45 -0700

    Models for list Item and associated migration

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -11,25 +12,46 @@ class HomePageTest(TestCase):
     	self.assertIn('A new list item', response.content.decode())
     	self.assertTemplateUsed(response,'home.html')
 	
-# refactoring of test_uses_home_template
-# from django.core.urlresolvers import resolve
-# from django.test import TestCase
-# from lists.views import home_page 
-# from django.http import HttpRequest 
+		# refactoring of test_uses_home_template
+		# from django.core.urlresolvers import resolve
+		# from django.test import TestCase
+		# from lists.views import home_page 
+		# from django.http import HttpRequest 
 
-# class HomePageTest(TestCase):
+		# class HomePageTest(TestCase):
 
-#     def test_root_url_resolves_to_home_page_view(self):
-#         found = resolve('/')  
-#         self.assertEqual(found.func, home_page)  
+		# def test_root_url_resolves_to_home_page_view(self):
+		#      found = resolve('/')  
+		#      self.assertEqual(found.func, home_page)  
 
-#     def test_home_page_returns_correct_html(self):
-#         request = HttpRequest()  
-#         response = self.client.get('/')  
-#         html = response.content.decode('utf8')  
-#         self.assertIn('<title>To-Do lists</title>', html)  
-#         self.assertTrue(html.endswith('</html>'))  
-#         self.assertTemplateUsed(response ,'home.html')
+		# def test_home_page_returns_correct_html(self):
+		#      request = HttpRequest()  
+		#      response = self.client.get('/')  
+		#      html = response.content.decode('utf8')  
+		#      self.assertIn('<title>To-Do lists</title>', html)  
+		#      self.assertTrue(html.endswith('</html>'))  
+		#      self.assertTemplateUsed(response ,'home.html')
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retriving_items(self):
+		first_item = Item()
+		first_item.text ="first saved item"
+		first_item.save()
+
+		second_item =Item()
+		second_item.text ="second saved item"
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text , 'first saved item')
+		self.assertEqual(second_saved_item.text , 'second saved item')
+
+
 
 
 

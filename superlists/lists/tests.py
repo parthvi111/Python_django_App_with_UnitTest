a62be540c4888bd4337b94f2de988a90e605eaf4
Parthvi Patel <parthvi@Parthvis-MacBook-Pro.local>
2017-03-19 18:15:01 -0700

    Create new list for each user

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item,List
 
 class HomePageTest(TestCase):
 
@@ -27,38 +27,48 @@ class HomePageTest(TestCase):
 		#      self.assertTemplateUsed(response ,'home.html')
 
 
-class ItemModelTest(TestCase):
-
-	def test_saving_and_retriving_items(self):
-		first_item = Item()
-		first_item.text ="first saved item"
-		first_item.save()
-
-		second_item =Item()
-		second_item.text ="second saved item"
-		second_item.save()
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(),2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text , 'first saved item')
-		self.assertEqual(second_saved_item.text , 'second saved item')
-
-
 class ListViewTest(TestCase):
+
+	def test_displays_all_items(self):
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response,'list.html')
 
-	def test_displays_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-		response = self.client.get('/lists/the-only-list-in-the-world/')
-		self.assertContains(response, 'itemey 1') 
-		self.assertContains(response, 'itemey 2')
+
+
+class ListandItemModelTest(TestCase):
+	def test_saving_and_retriving_items(self):
+		list_ = List()
+		list_.save()
+
+		first_item =Item()
+		first_item.text = "The first ITEM"
+		first_item.list =list_	
+		first_item.save()
+
+		second_item =Item()
+		second_item.text = "The second ITEM"
+		second_item.list =list_	
+		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list,list_)
+
+		saved_items =Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item =saved_items[1]
+		self.assertEqual(first_saved_item.list,list_)
+		self.assertEqual(second_saved_item.list,list_)
+		self.assertEqual(first_saved_item.text,'The first ITEM')
+		self.assertEqual(second_saved_item.text,'The second ITEM')
+
+
 
 class NewListTest(TestCase):
 
